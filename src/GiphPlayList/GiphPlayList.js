f3aaefc3f43071367a499d779df80dbbd9a3113f
Mhorvilleur13 <mhorvilleur13@gmail.com>
2023-06-13 11:27:28 -0600

    chagned search

--- src/GiphPlayList/GiphPlayList.js
+++ src/GiphPlayList/GiphPlayList.js
@@ -1,8 +1,9 @@
-export function GiphPlaylist({ giphPlaylist }) {
+export function GiphPlaylist({ giphPlaylist, removeGiph }) {
     return (
         giphPlaylist.map((giph, index) => {
             return (<div className="search-result">
                 <img src={giph} key={index} height="300px" width="300px" ></img>
+                <button onClick={() => removeGiph(giph)}>Remove From Playlist</button>
             </div>
             )
         })
--- src/util/Giph.js
+++ src/util/Giph.js
@@ -1,41 +1,44 @@
-
 const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.REACT_APP_API_KEY}`;
 
-
 const Giph = {
-    async getRandomGiph() {
-        try {
-            const response = await fetch(url);
-            if (response.ok) {
-                const jsonResponse = await response.json();
-                const giph = jsonResponse.data.images.original.url;
-                console.log(giph);
-                return giph
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    },
-
-    async getGiph(inputValue, amount) {
-        const urlToFetch = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${inputValue}&limit=${amount}`
-        try {
-            const response = await fetch(urlToFetch);
-            if (response.ok) {
-                const jsonResponse = await response.json();
-                console.log(jsonResponse);
-                // const giphies = jsonResponse.data[0].images.original.url;
-                const giphies = jsonResponse.data.map(item => item.images.original.url);
-                //const giphies = jsonResponse.data[0].images.original.url;
-                console.log(giphies);
-                return giphies;
-            }
-        } catch (error) {
-            console.log(error);
-        }
+  async getRandomGiph() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+        const giph = jsonResponse.data.images.original.url;
+        console.log(giph);
+        return giph;
+      }
+    } catch (error) {
+      console.log(error);
     }
+  },
 
-
-}
+  async getGiph(inputValue, amount) {
+    const urlToFetch = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.REACT_APP_API_KEY}&q=${inputValue}&limit=${amount}`;
+    try {
+      const response = await fetch(urlToFetch);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        console.log(jsonResponse);
+        const giphies = jsonResponse.data.map(
+          (item) => item.images.original.url
+        );
+        console.log(giphies);
+        return giphies;
+        /*let giphs = jsonResponse.data.map(giph => ({
+                    url: giph.images.original.url,
+                    id: giph.id
+                }))
+                console.log(giphs);
+                return giphs;
+                */
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  },
+};
 export default Giph;
-
